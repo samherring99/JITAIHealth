c530fafdc0b27f0141d68e1039d882b6a77a72af
Sam Herring <samherring99@gmail.com>
2020-07-31 09:53:58 -0400

    Initial Commit

---
+++ JITAIHealth WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  JITAIHealth WatchKit Extension
+//
+//  Created by Sam Herring on 7/31/20.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
